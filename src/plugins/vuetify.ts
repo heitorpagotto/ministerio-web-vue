739d6fcfdc72902e3695d6d8ac25ffe0ed96ff9b
heitorpagotto <heitor.pagotto@outlook.com>
2023-08-29 14:32:38 -0300

    added proj

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,15 @@
+// Vuetify
+import * as directives from 'vuetify/directives'
+import * as components from 'vuetify/components'
+import { createVuetify } from 'vuetify'
+
+// Styles
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  components,
+  directives
+})
+
+export default vuetify
